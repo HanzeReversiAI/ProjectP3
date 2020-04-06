f95cde2848ab59ec790a07da805917ccac55bb0e
ThowV <thomasvisscher@outlook.com>
2020-04-06 21:39:37 +0200

    Implement lobby panel switching

--- src/main/java/com/hanzereversiai/projectp3/ui/ConnectionPanelController.java
+++ src/main/java/com/hanzereversiai/projectp3/ui/ConnectionPanelController.java
@@ -1,6 +1,7 @@
 package com.hanzereversiai.projectp3.ui;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -13,12 +14,17 @@ public class ConnectionPanelController {
     public TextField usernameTextField;
     public Label errorLabel;
 
+    @FXML
+    public void initialize() {
+        connectionPanelModel = new ConnectionPanelModel();
+    }
+
     public void onConnectButtonActivated(ActionEvent actionEvent) {
         String result = connectionPanelModel.startOnlineConnection(
                 ipTextField.getText(), portTextField.getText(), usernameTextField.getText());
 
         if (result.isEmpty())
-            UIHelper.switchScene(actionEvent, "lobby-panel");
+            createLobbyPanel(actionEvent);
 
         errorLabel.setText(result);
     }
@@ -27,9 +33,13 @@ public class ConnectionPanelController {
         String result = connectionPanelModel.startOfflineConnection(usernameTextField.getText());
 
         if (result.isEmpty())
-            UIHelper.switchScene(actionEvent, "lobby-panel");
+            createLobbyPanel(actionEvent);
 
         errorLabel.setText(result);
+    }
+
+    private void createLobbyPanel(ActionEvent actionEvent) {
+        UIHelper.switchScene(actionEvent, "lobby-panel");
     }
 
     public void setModel(ConnectionPanelModel connectionPanelModel) {
--- src/main/java/com/hanzereversiai/projectp3/ui/LobbyPanelController.java
+++ src/main/java/com/hanzereversiai/projectp3/ui/LobbyPanelController.java
@@ -1,31 +1,58 @@
 package com.hanzereversiai.projectp3.ui;
 
 import com.hanzereversiai.projectp3.networking.NetworkSingleton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 
 public class LobbyPanelController {
+    Parent offlineLobbyPane;
+    Parent onlineLobbyPane;
+
+    public Button onlineButton;
+    public Button offlineButton;
     public StackPane contentStackPane;
     public Label welcomeLabel;
 
     @FXML
     public void initialize() {
         welcomeLabel.setText("Welcome back " + NetworkSingleton.getNetworkInstance().getUsername());
-        loadUI("offline-lobby-panel");
+        offlineLobbyPane = loadUI("offline-lobby-panel");
+
+        if (NetworkSingleton.getNetworkInstance().isConnected())
+            onlineLobbyPane = loadUI("online-lobby-panel");
     }
 
-    private void loadUI(String ui) {
+    public void onOnlineButtonActivated(ActionEvent actionEvent) {
+        if (onlineLobbyPane != null)
+            switchContentStackPaneContent(onlineLobbyPane);
+        else
+            UIHelper.switchScene(actionEvent, "connection-panel");
+    }
+
+    public void onOfflineButtonActivated(ActionEvent actionEvent) {
+        if (offlineLobbyPane != null)
+            switchContentStackPaneContent(offlineLobbyPane);
+    }
+
+    private Parent loadUI(String ui) {
         try {
-            Parent root = new FXMLLoader(getClass().getResource("/" + ui + ".fxml")).load();
-            contentStackPane.getChildren().clear();
-            contentStackPane.getChildren().add(root);
+            Parent content = new FXMLLoader(getClass().getResource("/" + ui + ".fxml")).load();
+            switchContentStackPaneContent(content);
+            return content;
         } catch (IOException e) {
-            e.printStackTrace();
+            return null;
         }
+    }
+
+    private void switchContentStackPaneContent(Parent content) {
+        contentStackPane.getChildren().clear();
+        contentStackPane.getChildren().add(content);
     }
 }
