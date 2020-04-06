88dbe8e829fe2b9d97c5617dfb2482fd3f0a700d
rizza99 <44263376+rizza99@users.noreply.github.com>
2020-04-06 20:01:18 +0200

    Update TTToeAdvancedAI.java
    werkt nog niet maar is minder kapot

--- src/main/java/com/hanzereversiai/projectp3/TTToeTurnAI/TTToeAdvancedAI.java
+++ src/main/java/com/hanzereversiai/projectp3/TTToeTurnAI/TTToeAdvancedAI.java
@@ -4,11 +4,15 @@ import com.thowv.javafxgridgameboard.AbstractGameInstance;
 import com.thowv.javafxgridgameboard.GameBoard;
 import com.thowv.javafxgridgameboard.GameBoardTile;
 import com.thowv.javafxgridgameboard.GameBoardTileType;
-import com.thowv.javafxgridgameboard.premades.AbstractTurnEntityRandomAI;
+import com.thowv.javafxgridgameboard.AbstractTurnEntity;
 import com.thowv.javafxgridgameboard.premades.tictactoe.TTToeAlgorithms;
 import java.util.ArrayList;
 
-public class TTToeAdvancedAI extends AbstractTurnEntityRandomAI {
+public class TTToeAdvancedAI extends AbstractTurnEntity {
+    public TTToeAdvancedAI() {
+        super(EntityType.AI);
+    }
+
     @Override
     public void takeTurn(AbstractGameInstance gameInstance) {
         ArrayList<GameBoardTile> possibleGameBoardTiles = gameInstance.getGameBoard().getTilesByType(
@@ -17,7 +21,10 @@ public class TTToeAdvancedAI extends AbstractTurnEntityRandomAI {
         gameInstance.getGameBoard().setTileTypes(possibleGameBoardTiles,
                 GameBoardTileType.VISIBLE);
         GameBoardTile best = findBestMove(gameInstance);
+        System.out.println(best.getXCord());
+        System.out.println(best.getYCord());
         gameInstance.doTurn(best.getXCord(),best.getYCord());
+
     }
 
 
@@ -26,7 +33,7 @@ public class TTToeAdvancedAI extends AbstractTurnEntityRandomAI {
     {
         if(gameInstance.getGameBoard().getTilesByType(GameBoardTileType.HIDDEN).size() > 0){
             return true;
-                }
+        }
         return false;
     }
 
@@ -34,9 +41,11 @@ public class TTToeAdvancedAI extends AbstractTurnEntityRandomAI {
     static int evaluate(GameBoard gameBoard) {
         if (TTToeAlgorithms.checkThreeInRow(gameBoard) == GameBoardTileType.PLAYER_1) {
             return +10;
-        } else if (TTToeAlgorithms.checkThreeInRow(gameBoard) == GameBoardTileType.PLAYER_2) {
+        }
+        else if (TTToeAlgorithms.checkThreeInRow(gameBoard) == GameBoardTileType.PLAYER_2) {
             return -10;
-        } else return 0;
+        }
+        else return 0;
     }
 
     //minMax func
@@ -54,20 +63,16 @@ public class TTToeAdvancedAI extends AbstractTurnEntityRandomAI {
             return 0;
 
         // If this maximizer's move
-        if (isMax)
-        {
+        if (isMax) {
             int best = -1000;
 
             // Traverse all cells
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
                     // Check if cell is empty
                     if (gameInstance.getGameBoard().getTile(i,j).getGameBoardTileType() != GameBoardTileType.PLAYER_1 && gameInstance.getGameBoard().getTile(i,j).getGameBoardTileType() != GameBoardTileType.PLAYER_2){
                         // Make the move
                         gameInstance.getGameBoard().setTileType(i,j,GameBoardTileType.PLAYER_1);
-
                         // Call minmax recursively and choose the maximum value
                         best = Math.max(best, minMax(gameInstance,depth + 1, !isMax));
                         // Undo the move
@@ -79,15 +84,11 @@ public class TTToeAdvancedAI extends AbstractTurnEntityRandomAI {
         }
 
         // If this minimizer's move
-        else
-        {
+        else {
             int best = 1000;
-
             // Traverse all cells
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
+            for (int i = 0; i < 3; i++){
+                for (int j = 0; j < 3; j++){
                     // Check if cell is empty
                     if (gameInstance.getGameBoard().getTile(i,j).getGameBoardTileType() != GameBoardTileType.PLAYER_1 && gameInstance.getGameBoard().getTile(i,j).getGameBoardTileType() != GameBoardTileType.PLAYER_2){
 
@@ -109,20 +110,18 @@ public class TTToeAdvancedAI extends AbstractTurnEntityRandomAI {
     static GameBoardTile findBestMove(AbstractGameInstance gameInstance)
     {
         int bestVal = -1000;
-
         int bestMoveX = -1;
         int bestMoveY = -1;
 
         // Traverse all cells, evaluate minmax function for all empty cells. And return the cell with optimal value.
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
                 // Check if cell is empty
                 if (gameInstance.getGameBoard().getTile(i,j).getGameBoardTileType() != GameBoardTileType.PLAYER_1 && gameInstance.getGameBoard().getTile(i,j).getGameBoardTileType() != GameBoardTileType.PLAYER_2){
 
                     // Make the move
                     gameInstance.getGameBoard().setTileType(i,j,GameBoardTileType.PLAYER_1);
+
                     // compute evaluation function for this move.
                     int moveVal = minMax(gameInstance, 0, false);
 
@@ -130,8 +129,7 @@ public class TTToeAdvancedAI extends AbstractTurnEntityRandomAI {
                     gameInstance.getGameBoard().setTileType(i,j,GameBoardTileType.HIDDEN);
 
                     // If the value of the current move is more than the best value, then update best
-                    if (moveVal > bestVal)
-                    {
+                    if (moveVal > bestVal) {
                         bestMoveX = i;
                         bestMoveY = j;
                         bestVal = moveVal;
