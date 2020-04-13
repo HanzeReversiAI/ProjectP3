53696211cf0c2dfd179a860b0699df06ba061a1f
Guido Lindeboom <guido@justweb.nu>
2020-04-13 14:43:24 +0200

    Added JavaDoc

--- src/main/java/com/hanzereversiai/projectp3/ai/ReversiTurnEntityAdvancedAI.java
+++ src/main/java/com/hanzereversiai/projectp3/ai/ReversiTurnEntityAdvancedAI.java
@@ -11,15 +11,27 @@ import java.util.ArrayList;
 
 public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
 
+
     private int[][] tileWeights;
     private int nOfMovesAhead;
 
+    /**
+     * Initiates ReversiTurnEntityAdvancedAI with the default nOfMovesAhead.
+     *
+     * @param name name of the player
+     */
     public ReversiTurnEntityAdvancedAI(String name)
     {
         // 5 moves ahead seems to be ideal (good combination of calculating far ahead but remaining realistic)
         this(name, 5);
     }
 
+    /**
+     * Initiates ReversiTurnEntityAdvancedAI with the given nOfMovesAhead.
+     *
+     * @param name name of the player
+     * @param nOfMovesAhead number of moves to calculate
+     */
     public ReversiTurnEntityAdvancedAI(String name, int nOfMovesAhead)
     {
         super(name);
@@ -28,12 +40,22 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         this.initiateWeights();
     }
 
+    /**
+     * Calls takeTurn with the appropriate GameInstance class.
+     *
+     * @param gameInstance the active game instance to calculate from
+     */
     @Override
     public void takeTurn(AbstractGameInstance gameInstance)
     {
         takeTurn((ReversiGameInstance)gameInstance);
     }
 
+    /**
+     * Handles the AI turn.
+     *
+     * @param gameInstance the active game instance to calculate from
+     */
     private void takeTurn(ReversiGameInstance gameInstance)
     {
         ArrayList<GameBoardTile> possibleGameBoardTiles = ReversiAlgorithms.determineTilePossibilities(
@@ -54,6 +76,12 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         pauseTransition.play();
     }
 
+    /**
+     * Performs the best possible move.
+     *
+     * @param gameInstance the active game instance to calculate from
+     * @param possibleMoves all currently possible moves
+     */
     private void doBestMove(ReversiGameInstance gameInstance, ArrayList<GameBoardTile> possibleMoves)
     {
         GameBoardTile bestMove = this.findBestMove(gameInstance, possibleMoves);
@@ -61,6 +89,13 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         gameInstance.doTurn(bestMove.getXCord(), bestMove.getYCord());
     }
 
+    /**
+     * Finds the best possible move.
+     *
+     * @param gameInstance the active game instance to calculate from
+     * @param possibleMoves all currently possible moves
+     * @return best possible move
+     */
     private GameBoardTile findBestMove(ReversiGameInstance gameInstance, ArrayList<GameBoardTile> possibleMoves)
     {
         GameBoardTileType currentTileType = gameInstance.getCurrentTurnEntity().getGameBoardTileType();
@@ -85,6 +120,16 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         return bestTileYet;
     }
 
+    /**
+     * Recursively calculates the obtained score after performing each possible move.
+     *
+     * @param gameBoardTiles 2d array of current board contents
+     * @param nOfMovesAhead number of moves to calculate
+     * @param nOfMovesProcessed number of moves already processed
+     * @param aiTurn if it's the AI's or not
+     * @param currentTileType tile type of the current player
+     * @return best possible score of out of all possible moves
+     */
     private int getNextMoveScore(GameBoardTile[][] gameBoardTiles, int nOfMovesAhead, int nOfMovesProcessed, boolean aiTurn, GameBoardTileType currentTileType){
         ArrayList<GameBoardTile> possibleMoves = ReversiAlgorithms.determineTilePossibilities(gameBoardTiles, currentTileType);
 
@@ -139,6 +184,14 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         return bestScoreYet;
     }
 
+    /**
+     * Gets the score after the specified move.
+     *
+     * @param gameBoardTiles 2d array of current board contents
+     * @param move move to perform
+     * @param currentTileType tile type of the current player
+     * @return score after move.
+     */
     private int getScoreAfterMove(GameBoardTile[][] gameBoardTiles, GameBoardTile move, GameBoardTileType currentTileType)
     {
         int scoreOldBoard = this.evaluateBoard(gameBoardTiles, currentTileType);
@@ -149,6 +202,13 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         return scoreNewBoard - scoreOldBoard;
     }
 
+    /**
+     * Calculates the net score of the current player compared to the opponent.
+     *
+     * @param gameBoardTiles 2d array of current board contents
+     * @param currentTileType tile type of the current player
+     * @return net score of current player compared to opponent
+     */
     private int evaluateBoard(GameBoardTile[][] gameBoardTiles, GameBoardTileType currentTileType)
     {
         int scoreCurrentMinusOpponent = 0;
@@ -166,6 +226,9 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         return scoreCurrentMinusOpponent;
     }
 
+    /**
+     * Initiates the weight of each tile.
+     */
     private void initiateWeights()
     {
         int[][] weights = {
@@ -182,6 +245,12 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         this.tileWeights = weights;
     }
 
+    /**
+     * Gets a 2d array of the current board.
+     *
+     * @param gameBoardTiles 2d array of the current board contents
+     * @return 2d array of the current board contents as a new object
+     */
     private GameBoardTileType[][] getResetArray(GameBoardTile[][] gameBoardTiles)
     {
         GameBoardTileType[][] resetArray = new GameBoardTileType[8][8];
@@ -209,6 +278,12 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         return resetArray;
     }
 
+    /**
+     * Resets the board to the given 2d array.
+     *
+     * @param gameBoardTiles 2d array of the board
+     * @param resetArray 2d array to reset the board to
+     */
     private void resetBoardTo(GameBoardTile[][] gameBoardTiles, GameBoardTileType[][] resetArray)
     {
         for(int x = 0; x < 8; x++){
@@ -218,6 +293,12 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         }
     }
 
+    /**
+     * Checks whether the next move is the final move.
+     *
+     * @param gameBoardTiles 2d array of the current board contents
+     * @return true if final move, false if not
+     */
     private boolean isFinalMove(GameBoardTile[][] gameBoardTiles)
     {
         int nOfPlayerOccupiedTiles = this.getNOfTilesByType(gameBoardTiles, GameBoardTileType.PLAYER_1);
@@ -226,6 +307,13 @@ public class ReversiTurnEntityAdvancedAI extends AbstractTurnEntityRandomAI {
         return nOfPlayerOccupiedTiles <= 1;
     }
 
+    /**
+     * Gets the number of tiles of specified type.
+     *
+     * @param gameBoardTiles 2d array of current board contents
+     * @param gameBoardTileType type to find
+     * @return number of tiles of specified type
+     */
     private int getNOfTilesByType(GameBoardTile[][] gameBoardTiles, GameBoardTileType gameBoardTileType)
     {
         int n = 0;
